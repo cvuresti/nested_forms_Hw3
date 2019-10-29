a1a9b645ec04e2894bd5ef5e766fcc36dce40f05
Chadwick Uresti <cvuresti@uh.edu>
2019-10-28 23:13:20 -0500

    initial commit

---
+++ app/views/offices/_office.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! office, :id, :address, :, :zip, :string, :phone, :, :created_at, :updated_at
+json.url office_url(office, format: :json)
---
+++ db/migrate/20191029015210_create_physicians.rb
@@ -0,0 +1,12 @@
+class CreatePhysicians < ActiveRecord::Migration[6.0]
+  def change
+    create_table :physicians do |t|
+      t.string :name
+      t.string :specialty
+      t.belongs_to :appointments, null: false, foreign_key: true
+
+      t.timestamps
+      
+    end
+  end
+end
